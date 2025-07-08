9c30e0d1838ad046dfdacd94aedfcd455b0f4d4e
monsieur486 <monsieur486@gmail.com>
2025-07-08 16:22:18 +0200

    add javadoc

--- src/test/java/com/mr486/safetynet/SafetyNetApplicationTests.java
+++ src/test/java/com/mr486/safetynet/SafetyNetApplicationTests.java
@@ -3,9 +3,18 @@ package com.mr486.safetynet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Test class for the Spring Boot SafetyNet application.
+ * <p>
+ * This class verifies that the application context loads successfully.
+ * </p>
+ */
 @SpringBootTest
 public class SafetyNetApplicationTests {
 
+  /**
+   * Verifies that the Spring Boot application context loads without errors.
+   */
   @Test
   void contextLoads() {
   }
