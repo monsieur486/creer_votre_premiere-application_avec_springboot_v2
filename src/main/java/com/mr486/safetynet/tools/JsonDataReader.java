059ad284b85bb784dd9a7f60f3f6b5f3c112de87
monsieur486 <monsieur486@gmail.com>
2025-07-08 16:30:19 +0200

    add class JsonDataReader.java

---
+++ src/main/java/com/mr486/safetynet/tools/JsonDataReader.java
@@ -0,0 +1,50 @@
+package com.mr486.safetynet.tools;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mr486.safetynet.dto.DataBindingDto;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+/**
+ * Component responsible for loading data from a JSON file.
+ * Utilizes Jackson's ObjectMapper to deserialize JSON into a DataBinding object.
+ */
+@Slf4j
+@Component
+@Data
+@RequiredArgsConstructor
+public class JsonDataReader {
+
+  /**
+   * The ObjectMapper instance used for JSON deserialization.
+   * This is a required dependency injected via the constructor.
+   */
+  private final ObjectMapper mapper;
+
+  /**
+   * The path to the JSON file, injected from application properties.
+   */
+  @Value("${json.file.path}")
+  String dataFilePath;
+
+  /**
+   * Loads data from a specified JSON file path and maps it to a DataBinding object.
+   *
+   * @return a DataBinding object containing the deserialized data
+   * @throws RuntimeException if the file is not found or an error occurs while reading the file
+   */
+  public DataBindingDto loadData() {
+
+    try {
+      File file = new File(dataFilePath);
+      return mapper.readValue(file, DataBindingDto.class);
+    } catch (Exception e) {
+      throw new RuntimeException("Error reading json file:" + dataFilePath + " message: " + e.getMessage());
+    }
+  }
+}
