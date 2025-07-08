f52056aa8c2a33212d0e85a7025a4d30a08af7ef
monsieur486 <monsieur486@gmail.com>
2025-07-08 18:55:36 +0200

    add FireStationController

---
+++ src/main/java/com/mr486/safetynet/controller/FireStationController.java
@@ -0,0 +1,63 @@
+package com.mr486.safetynet.controller;
+
+import com.mr486.safetynet.model.FireStation;
+import com.mr486.safetynet.service.FireStationService;
+import com.mr486.safetynet.tools.ResponseUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Controller for managing FireStation-related operations.
+ * Provides endpoints for CRUD operations on FireStation entities.
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/firestation")
+public class FireStationController {
+
+  private final FireStationService fireStationService;
+
+  /**
+   * Adds a new fire station.
+   *
+   * @param fireStation The FireStation object to be added. Must be valid.
+   * @return ResponseEntity containing a success message.
+   */
+  @PostMapping(path = "", produces = "application/json")
+  public ResponseEntity<String> addFireStation(@RequestBody @Valid FireStation fireStation) {
+    fireStationService.saveFireStation(fireStation);
+    return ResponseUtil.created(
+            "Fire station added successfully"
+    );
+  }
+
+  /**
+   * Updates an existing fire station.
+   *
+   * @param fireStation The FireStation object with updated information. Must be valid.
+   * @return ResponseEntity containing a success message.
+   */
+  @PutMapping(path = "", produces = "application/json")
+  public ResponseEntity<String> updateFireStation(@RequestBody @Valid FireStation fireStation) {
+    fireStationService.updateFireStation(fireStation);
+    return ResponseUtil.created(
+            "Fire station updated successfully"
+    );
+  }
+
+  /**
+   * Deletes a fire station by its address.
+   *
+   * @param address The address of the fire station to be deleted. Must be valid.
+   * @return ResponseEntity containing a success message.
+   */
+  @DeleteMapping(path = "", produces = "application/json")
+  public ResponseEntity<String> deleteFireStationByAddress(@RequestBody @Valid String address) {
+    fireStationService.deleteFireStationByAddress(address);
+    return ResponseUtil.success(
+            "Fire station deleted successfully for address: " + address
+    );
+  }
+}
