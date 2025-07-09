d827544728791c02d8317d3619a2a05108fd9013
monsieur486 <monsieur486@gmail.com>
2025-07-09 23:55:14 +0200

    add MedicalRecordController

---
+++ src/main/java/com/mr486/safetynet/controller/MedicalRecordController.java
@@ -0,0 +1,64 @@
+package com.mr486.safetynet.controller;
+
+import com.mr486.safetynet.dto.MedicalRecordDto;
+import com.mr486.safetynet.model.MedicalRecord;
+import com.mr486.safetynet.service.MedicalRecordService;
+import com.mr486.safetynet.tools.ResponseUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Controller for managing MedicalRecord-related operations.
+ * Provides endpoints for CRUD operations on MedicalRecord entities.
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/medicalrecord")
+public class MedicalRecordController {
+
+  private final MedicalRecordService medicalRecordService;
+
+  /**
+   * Adds a new medical record.
+   *
+   * @param medicalRecord The MedicalRecord object to be added. Must be valid.
+   * @return ResponseEntity containing a success message.
+   */
+  @PostMapping(path = "", produces = "application/json")
+  public ResponseEntity<String> addMedicalRecord(@RequestBody @Valid MedicalRecord medicalRecord) {
+    medicalRecordService.saveMedicalRecord(medicalRecord);
+    return ResponseUtil.created(
+            "Medical record added successfully"
+    );
+  }
+
+  /**
+   * Updates an existing medical record.
+   *
+   * @param medicalRecord The MedicalRecord object with updated information. Must be valid.
+   * @return ResponseEntity containing a success message.
+   */
+  @PutMapping(path = "/update", produces = "application/json")
+  public ResponseEntity<String> updateMedicalRecord(@RequestBody @Valid MedicalRecord medicalRecord) {
+    medicalRecordService.updateMedicalRecord(medicalRecord);
+    return ResponseUtil.created(
+            "Medical record updated successfully"
+    );
+  }
+
+  /**
+   * Deletes a medical record by its first and last name.
+   *
+   * @param medicalRecordDto The MedicalRecordDto object containing the first and last name of the medical record to be deleted. Must be valid.
+   * @return ResponseEntity containing a success message.
+   */
+  @DeleteMapping(path = "", produces = "application/json")
+  public ResponseEntity<String> deleteMedicalRecord(@RequestBody @Valid MedicalRecordDto medicalRecordDto) {
+    medicalRecordService.deleteMedicalRecord(medicalRecordDto);
+    return ResponseUtil.success(
+            "Medical record deleted successfully"
+    );
+  }
+}
