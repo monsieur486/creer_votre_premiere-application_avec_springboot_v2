57390a7c6c37fbc0496c8c3d55bbd8158d9577db
monsieur486 <monsieur486@gmail.com>
2025-07-09 23:34:00 +0200

    add javadoc

--- src/main/java/com/mr486/safetynet/service/MedicalRecordService.java
+++ src/main/java/com/mr486/safetynet/service/MedicalRecordService.java
@@ -10,16 +10,30 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/**
+ * Service class for managing medical records.
+ * Provides methods to retrieve, save, update, and delete medical records.
+ */
 @Service
 @RequiredArgsConstructor
 public class MedicalRecordService {
 
   private final MedicalRecordRepository medicalRecordRepository;
 
+  /**
+   * Retrieves a medical record by first name and last name.
+   *
+   * @param medicalRecordDto the DTO containing first name and last name
+   * @return an Optional containing the MedicalRecord if found, otherwise empty
+   */
   public Optional<MedicalRecord> getMedicalRecordByFirstNameAndLastName(MedicalRecordDto medicalRecordDto) {
     return medicalRecordRepository.findByFirstNameAndLastName(medicalRecordDto);
   }
 
+  /**
+   * Retrieves a medical record by first name and last name.
+   * @param medicalRecord the MedicalRecord object containing first name and last name
+   */
   public void saveMedicalRecord(MedicalRecord medicalRecord) {
     MedicalRecordDto medicalRecordDto = new MedicalRecordDto(medicalRecord.getFirstName(), medicalRecord.getLastName());
     if (medicalRecordRepository.exists(medicalRecordDto)) {
@@ -29,6 +43,11 @@ public class MedicalRecordService {
     medicalRecordRepository.save(medicalRecord);
   }
 
+  /**
+   * Updates an existing medical record.
+   *
+   * @param medicalRecord the MedicalRecord object containing updated information
+   */
   public void updateMedicalRecord(MedicalRecord medicalRecord) {
     MedicalRecordDto medicalRecordDto = new MedicalRecordDto(medicalRecord.getFirstName(), medicalRecord.getLastName());
     if (!medicalRecordRepository.exists(medicalRecordDto)) {
@@ -38,6 +57,11 @@ public class MedicalRecordService {
     medicalRecordRepository.update(medicalRecord);
   }
 
+  /**
+   * Deletes a medical record.
+   *
+   * @param medicalRecordDto the DTO containing first name and last name of the medical record to delete
+   */
   public void deleteMedicalRecord(MedicalRecordDto medicalRecordDto) {
     if (!medicalRecordRepository.exists(medicalRecordDto)) {
       String message = "Medical record for first name [" + medicalRecordDto.getFirstName() + "] and last name [" + medicalRecordDto.getLastName() + "] does not exist!";
