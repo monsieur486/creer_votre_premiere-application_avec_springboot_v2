960206124fb3fbd806702f03d49bd3fbd0c7aef5
monsieur486 <monsieur486@gmail.com>
2025-07-11 12:49:26 +0200

    reformat code

--- src/main/java/com/mr486/safetynet/exeption/WrongDateFormatException.java
+++ src/main/java/com/mr486/safetynet/exeption/WrongDateFormatException.java
@@ -4,14 +4,12 @@ package com.mr486.safetynet.exeption;
  * Exception thrown when a date format is incorrect.
  * This custom exception is used to indicate errors related to invalid date formats.
  */
-public class WrongDateFormatException extends RuntimeException{
+public class WrongDateFormatException extends RuntimeException {
 
   /**
    * Constructs a new WrongDateFormatException with the specified detail message.
    *
    * @param message the detail message explaining the reason for the exception
    */
-  public WrongDateFormatException(String message) {
-    super(message);
-  }
+  public WrongDateFormatException(String message) { super(message); }
 }
